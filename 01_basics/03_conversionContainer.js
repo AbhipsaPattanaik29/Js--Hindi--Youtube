73c3b19495912f1a706d6eab5f332cabe22c8b11
Abhipsa Pattanaik <pattanaikabhipsa29@gmail.com>
2025-03-17 15:16:07 +0000

    conversion of data types

---
+++ 01_basics/03_conversionContainer.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+//if want to do operations on a number
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//33 => 33
+//"33abc" => NaN
+//true=>1; false=>0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+//if 1 => output :true; 0 => false
+//""=>false
+// "abhipsa" => true
+
+let someNumber = 33
+let StringNumber = String(someNumber);
+console.log(typeof StringNumber);
+console.log(StringNumber);
