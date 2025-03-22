087f187cd1998d860035a4185838396912cba02f
Abhipsa Pattanaik <pattanaikabhipsa29@gmail.com>
2025-03-22 10:36:58 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+//console.log(2 > 1)
+//console.log(2 >= 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) 
+
+//these comparison such as ==, >=, > can confuse us
+
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+// //undefined always give false
+
+// === => strict check
+//check values as well as its data types
+
+
+console.log("2" == 2) // true
+console.log("2" === 2) // false
